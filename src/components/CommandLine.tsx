35dd33b2955c46500fc94764426b881452d616de
Michael Chen <michaelchengaming@gmail.com>
2020-08-10 13:24:12 -0700

    add focus trapper for commandline and implement app state chagne through cmdline

--- src/components/CommandLine.tsx
+++ src/components/CommandLine.tsx
@@ -1,5 +1,6 @@
 import React, { useRef, useLayoutEffect } from "react";
 import { useClickOutside } from "../hooks/useClickOutside";
+import FocusTrap from "focus-trap-react";
 
 type MenuProps = {
   showMenu: React.Dispatch<React.SetStateAction<boolean>>;
@@ -16,9 +17,16 @@ const CommandLine = ({ showMenu, setThing }: MenuProps) => {
   const insideNode = useClickOutside(() => showMenu(false));
 
   const handleKeyPress = (e: React.KeyboardEvent<HTMLDivElement>) => {
-    if (e.key === "Enter") {
-      console.log("poop");
-      console.log(e);
+    if (e.key !== "Enter") return;
+    e.preventDefault();
+    console.log(e.currentTarget.id);
+    if (e.currentTarget.id === "intro") {
+      setThing("intro");
+      showMenu(false);
+    }
+    if (e.currentTarget.id === "user") {
+      setThing("user-form");
+      showMenu(false);
     }
   };
 
@@ -26,23 +34,25 @@ const CommandLine = ({ showMenu, setThing }: MenuProps) => {
     <div className="menu-modal">
       <form ref={insideNode}>
         <input ref={input} type="text" placeholder="Type " tabIndex={0} />
-        <div className="selections">
-          <div tabIndex={0} onKeyDown={(e) => handleKeyPress(e)}>
-            thiing
+        <FocusTrap>
+          <div className="selections">
+            <div tabIndex={0} onKeyDown={handleKeyPress} id="intro">
+              Return to Main Menu
+            </div>
+            <div tabIndex={0} onKeyDown={handleKeyPress} id="user">
+              Edit User Stats
+            </div>
+            <div tabIndex={0} onKeyDown={handleKeyPress} id="calc-1">
+              TDEE Calculator
+            </div>
+            <div tabIndex={0} onKeyDown={handleKeyPress}>
+              thiing
+            </div>
+            <div tabIndex={0} onKeyDown={handleKeyPress}>
+              thiing
+            </div>
           </div>
-          <div tabIndex={0} onKeyDown={handleKeyPress}>
-            thiing
-          </div>
-          <div tabIndex={0} onKeyDown={handleKeyPress}>
-            thiing
-          </div>
-          <div tabIndex={0} onKeyDown={handleKeyPress}>
-            thiing
-          </div>
-          <div tabIndex={0} onKeyDown={handleKeyPress}>
-            thiing
-          </div>
-        </div>
+        </FocusTrap>
         {/* <select>
           <option value="">sdfjksdkjfhsjdkf</option>
         </select> */}
